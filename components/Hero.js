b45c51ad21f9b7a13fce0be9d6c26f43d0078b30
yeul <mpscqw@gmail.com>
2019-07-23 21:34:14 -0400

    Fixed device responsiveness of the hero image section of the site.

--- components/Hero.js
+++ components/Hero.js
@@ -3,14 +3,20 @@ import React, { Fragment } from "react";
 const Hero = () => {
   return (
     <Fragment>
-      <div className='hero-transition-images'>
-        <img className='hero-mars-surface-1' src='../static/images/hero-mars-surface-1.png' />
-        <img className='hero-mars-surface-2' src='../static/images/hero-mars-surface-2.png' />
-        <img className='hero-mars-surface-3' src='../static/images/hero-mars-surface-3.png' />
-      </div>
-      <div className='row no-gutters'>
-        <div className='landing col-lg-12'>
-          <h1>Planum Australe</h1>
+      <div className='hero-container container-fluid'>
+        <div className='hero-row row no-gutters'>
+          <div className='hero-images-col col-lg-12'>
+            <div className='hero-transition-images'>
+              <img className='hero-mars-surface-1' src='../static/images/hero-mars-surface-1.png' />
+              <img className='hero-mars-surface-2' src='../static/images/hero-mars-surface-2.png' />
+              <img className='hero-mars-surface-3' src='../static/images/hero-mars-surface-3.png' />
+            </div>
+            <div className='hero-title-row row no-gutters'>
+              <div className='hero-title-col col-lg-12'>
+                <h1 className='hero-title'>Planum Australe</h1>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Fragment>
