73f6c60b817db93ddd116447415d34947c65457f
yeul <mpscqw@gmail.com>
2019-07-25 18:57:48 -0400

    Reordered jsx elements depending upon device size using react-responsive.  Fixed spacing between elements.

---
+++ components/SmallDevices.js
@@ -0,0 +1,62 @@
+import React, { Fragment } from "react";
+import MediaQuery from "react-responsive";
+
+import "../styles/main.scss";
+
+class SmallDevices extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <MediaQuery query='(max-width: 767.98px)'>
+          <div className='deimos row no-gutters'>
+            <div className='deimos-image-col col-lg-6'>
+              <img
+                data-aos='fade-right'
+                data-aos-offset='200'
+                data-aos-delay='50'
+                data-aos-duration='1000'
+                data-aos-easing='ease-in-out'
+                className='deimos-moon-img'
+                src='../static/images/Deimos.png'
+              />
+            </div>
+            <div className='deimos-info-col col-lg-6'>
+              <h2 className='deimos-title'>
+                <span>Deimos</span>
+              </h2>
+
+              <p className='deimos-info'>
+                Deimos (systematic designation: Mars II) is the smaller and outermost of the two natural satellites of the planet Mars. Deimos has a mean radius of 6.2 km (3.9 mi)
+                and takes 30.3 hours to orbit Mars.
+              </p>
+            </div>
+          </div>
+          <div className='phobos row no-gutters'>
+            <div className='phobos-image-col col-lg-6'>
+              <img
+                data-aos='fade-left'
+                data-aos-offset='200'
+                data-aos-delay='50'
+                data-aos-duration='1000'
+                data-aos-easing='ease-in-out'
+                className='phobos-moon-img'
+                src='../static/images/Phobos.png'
+              />
+            </div>
+            <div className='phobos-info-col col-lg-6'>
+              <h2 className='phobos-title'>
+                <span>PHOBOS</span>
+              </h2>
+              <p className='phobos-info'>
+                Phobos is a small, irregularly shaped object with a mean radius of 11 km (7 mi) and is seven times as massive as the outer moon, Deimos. Phobos is named after the
+                Greek god Phobos, a son of Ares (Mars) and Aphrodite (Venus) and the personification of fear.
+              </p>
+            </div>
+          </div>
+        </MediaQuery>
+      </Fragment>
+    );
+  }
+}
+
+export default SmallDevices;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import React from "react";
+import MediaQuery from "react-responsive";
 
 import ScrollTop from "../components/ScrollTop";
 import Hero from "../components/Hero";
 import Card from "../components/Card";
+import SmallDevices from "../components/SmallDevices";
+import LargeDevices from "../components/LargeDevices";
 import "../styles/main.scss";
 // import "../styles/astronaut.scss";
 
@@ -29,51 +32,8 @@ class App extends React.Component {
         <Hero />
         <div className='explore-mars-container container-fluid'>
           <div className='row no-gutters'>
-            <div className='deimos row no-gutters'>
-              <div className='deimos-image-col col-lg-6'>
-                <img
-                  data-aos='fade-right'
-                  data-aos-offset='200'
-                  data-aos-delay='50'
-                  data-aos-duration='1000'
-                  data-aos-easing='ease-in-out'
-                  className='deimos-moon-img'
-                  src='../static/images/Deimos.png'
-                />
-              </div>
-              <div className='deimos-info-col col-lg-6'>
-                <h2 className='deimos-title'>
-                  <span>Deimos</span>
-                </h2>
-
-                <p className='deimos-info'>
-                  Deimos (systematic designation: Mars II) is the smaller and outermost of the two natural satellites of the planet Mars. Deimos has a mean radius of 6.2 km (3.9
-                  mi) and takes 30.3 hours to orbit Mars.
-                </p>
-              </div>
-            </div>
-            <div className='phobos row no-gutters'>
-              <div className='phobos-info-col col-lg-6'>
-                <h2 className='phobos-title'>
-                  <span>PHOBOS</span>
-                </h2>
-                <p className='phobos-info'>
-                  Phobos is a small, irregularly shaped object with a mean radius of 11 km (7 mi) and is seven times as massive as the outer moon, Deimos. Phobos is named after the
-                  Greek god Phobos, a son of Ares (Mars) and Aphrodite (Venus) and the personification of fear.
-                </p>
-              </div>
-              <div className='phobos-image-col col-lg-6'>
-                <img
-                  data-aos='fade-left'
-                  data-aos-offset='200'
-                  data-aos-delay='50'
-                  data-aos-duration='1000'
-                  data-aos-easing='ease-in-out'
-                  className='phobos-moon-img'
-                  src='../static/images/Phobos.png'
-                />
-              </div>
-            </div>
+            <LargeDevices />
+            <SmallDevices />
             <div className='col-lg-12'>
               <div className='explore-mars'>
                 <div className='about-mars-left'>
